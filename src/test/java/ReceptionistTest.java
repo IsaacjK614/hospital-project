9141ee8d25916c8a24daed9dafb7a1d4cdf00713
Isaacjk614 <isaac@DESKTOP-3F5L67V.localdomain>
2019-05-27 14:12:48 -0400

    Receptionist

---
+++ src/test/java/ReceptionistTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ReceptionistTest {
+
+	Receptionist underTestReceptionist = new Receptionist();
+
+	@Test
+	public void shouldHaveReceptionistSalaryOf45000() {
+		int exepectedReceptionistSalary = underTestReceptionist.getReceptionistSalary();
+		assertEquals(45000, exepectedReceptionistSalary);
+		
+	}
+	@Test
+	public void shouldHaveDefaultOnThePhoneAsFalse() {
+		Receptionist underTestReceptionist = new Receptionist();
+		boolean expectedOnThePhone = underTestReceptionist.getOnThePhone();
+		assertEquals(false, expectedOnThePhone);
+		
+	}
+	@Test
+	public void shouldBeAbleToPickUpPhoneWhichMakesOnThePhoneTrue() {
+		Receptionist underTestReceptionist = new Receptionist();
+		underTestReceptionist.onThePhone();
+		boolean expectedOnThePhone = underTestReceptionist.getOnThePhone();
+		assertEquals(true, expectedOnThePhone);
+		
+	}
+}
