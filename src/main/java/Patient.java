14088eec6ba8dbe770b993bc95dbc39f7a670f68
Isaacjk614 <isaac@DESKTOP-3F5L67V.localdomain>
2019-05-27 16:46:32 -0400

    update patient

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -2,17 +2,31 @@
 public class Patient {
 
 	private int bloodLevel = 20;
+	private int healthLevel = 10;
 
 	public int getBloodLevel() {
 		return bloodLevel ;
 	}
 
 	public int getHealthLevel() {
-		return 10;
+		return healthLevel;
 	}
 
-	public void drawBlood() {
-		int bloodLevel;
+	public void doctorDrawBlood() {
+		this.bloodLevel = this.bloodLevel - 5;
+	}
+
+	public void doctorCares() {
+		this.healthLevel = this.healthLevel + 10;
+		
+	}
+
+	public void nurseDrawBlood() {
+		this.bloodLevel = this.bloodLevel - 2;
+	}
+
+	public void nurseCares() {
+		this.healthLevel = this.healthLevel + 5;
 	}
 
 
